3dbe01880c6ba54a34068e1f5034677860d57b4e
root <root@pcuser.(none)>
2012-12-17 17:52:18 +0200

    16.12.12

--- Controller/Invoice/InvoiceController.php
+++ Controller/Invoice/InvoiceController.php
@@ -9,6 +9,10 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use HOffice\AdminBundle\Entity\Invoice\Invoice;
 use HOffice\AdminBundle\Form\Invoice\InvoiceType;
+use HOffice\AdminBundle\Form\Invoice\SearchInvoiceType;
+use Itc\AdminBundle\Tools\TranslitGenerator;
+use Symfony\Component\Locale\Locale;
+use Itc\AdminBundle\Tools\LanguageHelper;
 
 /**
  * Invoice\Invoice controller.
@@ -20,20 +24,167 @@ class InvoiceController extends Controller
     /**
      * Lists all Invoice\Invoice entities.
      *
-     * @Route("/", name="invoice")
+     * @Route("/{coulonpage}/{page}", name="invoice",
+     * requirements={"coulonpage" = "\d+","page" = "\d+"}, 
+     * defaults={ "coulonpage" = "100", "page" = 1} )
      * @Template()
-     */
-    public function indexAction()
+     */    
+    public function indexAction($coulonpage = 100, $page)
     {
         $em = $this->getDoctrine()->getManager();
+        $locale =  LanguageHelper::getLocale();
+        
+        $date_range = array();
+        
+        $repo = $em->getRepository('HOfficeAdminBundle:Invoice\Invoice');
+        
+        $qb = $repo->createQueryBuilder('I')
+                        ->select('I')
+                        ->orderBy('I.id', 'DESC');    
 
-        $entities = $em->getRepository('HOfficeAdminBundle:Invoice\Invoice')->findAll();
+        $paginator = $this->get('knp_paginator');
+        $entities = $paginator->paginate(
+            $qb,
+            $this->get('request')->query->get('page', 1)/*page number*/,
+            $coulonpage/*limit per page*/
+        );
+        $search_form = $this->createForm(new SearchInvoiceType($em, $locale));
 
         return array(
             'entities' => $entities,
+            'search_form' => $search_form->createView(),
+            'parent_id' => null,
+            'locale'    => $locale,
+            'coulonpage' => $coulonpage,            
+            'total' => null,            
         );
     }
+    /**
+     * @Route("/closed_month", name="closed_month")
+     */
+    public function сlosedMonthAction(Request $request){
+        
+        echo "wow";
+        $em = $this->getDoctrine()->getManager();
+        $locale =  LanguageHelper::getLocale();
 
+        $date_range = array();
+        
+        $first_day = date("Y")."-".(date("m")-1)."-01";
+        $last_day = date("Y")."-".(date("m")-1)."-31";
+        
+        $repo = $em->getRepository('HOfficeAdminBundle:Invoice\Invoice');
+        
+        $qb = $repo->createQueryBuilder('I')
+                        ->select('I')
+                        ->where("I.date >= :first_day AND I.date <= :last_day")
+                        ->setParameter('first_day', $first_day)
+                        ->setParameter('last_day', $last_day)
+                        ->orderBy('I.id', 'DESC');    
+        
+       $entities = $qb->getQuery()->execute();
+       
+       return $this->redirect($this->generateUrl('invoice'));
+    }
+    
+    /**
+     * @Route("/{coulonpage}/search", name="invoice_search",
+     * requirements={"coulonpage" = "\d+"}, 
+     * defaults={"coulonpage" = "100"})
+     * @Template("HOfficeAdminBundle:Invoice\Invoice:index.html.twig")
+     */
+    public function searchAction(Request $request, $coulonpage = 100)
+    {
+        $locale =  LanguageHelper::getLocale();
+        $em = $this->getDoctrine()->getManager();        
+        
+        $postData = $request->request->get('itc_documentsbundle_searchinvoicetype');
+        $house_id = $postData['house_id'];
+        
+        $search_form = $this->createForm(new SearchInvoiceType($em, $locale, $house_id));
+        $search_form->bind($request);
+        $deleteForm = array();   
+        $visibleForm = array();
+        $changeKodForm = array();   
+        $summa1 = 0 ;
+        $data = $search_form->getData();
+        
+        $repo = $em->getRepository('HOfficeAdminBundle:Invoice\Invoice');
+        
+        $qb = $repo->createQueryBuilder('I')
+                        ->select('I')
+                        ->orderBy('I.id', 'DESC');            
+
+        if(!is_null($data["house_id"]) || !is_null($data["user_id"]))
+            $qb->innerJoin('I.contract', 'C');
+        if(!is_null($data["user_id"]))
+        {
+            $qb->innerJoin('C.user', 'U', 
+                       'WITH', 'U.id = :user')
+               ->setParameter('user', $data["user_id"]);
+        }
+        if(!is_null($data["house_id"]))
+        {
+            $qb->innerJoin('C.apartment', 'A', 
+                    'WITH', 'A.house = :house')
+               ->setParameter('house', $data["house_id"]);
+        
+            if(!is_null($data["apartment_id"]))
+            {
+                $qb->andWhere('A.id = :apartment_id')
+                   ->setParameter('apartment_id', $data["apartment_id"]);
+            }
+        }
+        
+        if(!is_null($data["id"]))
+        {
+            $qb->orWhere('I.id = :id')
+                ->setParameter('id', $data["id"])
+                ;
+        }
+        if(!is_null($data["serial_number"]))
+        {
+            $qb->orWhere('I.N = :serial_number')
+                ->setParameter('serial_number', $data["serial_number"])
+                ;
+        }
+        if(!is_null($data["month"]))
+        {
+            $qb->orWhere('SUBSTRING(I.date,6,7) = :month')
+                ->setParameter('month', $data["month"])
+                ;
+        }
+        if(!is_null($data["year"]))
+        {
+            $qb->orWhere('SUBSTRING(I.date,1,4) = :year')
+                ->setParameter('year', $data["year"])
+                ;
+        }
+
+        $paginator = $this->get('knp_paginator');
+        
+        $entities = $paginator->paginate(
+            $qb,
+            $this->get('request')->query->get('page', 1),
+            100
+        );
+        foreach ($entities as $entity){
+            $summa1 += $entity->getSumma1();
+            $deleteForm[$entity->getId()] = $this->createDeleteForm($entity->getId())
+                            ->createView();
+        }
+        return array(
+            'entities'  => $entities,
+            'locale'    => $locale,
+            'parent_id' => null,
+            'chmap'     => array(),
+            'search_form' => $search_form->createView(),
+            'delete_form' => $deleteForm,
+            'coulonpage' => $coulonpage,
+            'total' => array('summa1' => $summa1),
+        );    
+    }
+    
     /**
      * Finds and displays a Invoice\Invoice entity.
      *
@@ -196,4 +347,5 @@ class InvoiceController extends Controller
             ->getForm()
         ;
     }
+            
 }
--- Form/Contract/ContractType.php
+++ Form/Contract/ContractType.php
@@ -11,89 +11,25 @@ use Doctrine\ORM\EntityRepository;
 
 class ContractType extends AbstractType
 {
-    private $house_id;
     /**
      *
      * @var Doctrine\ORM\EntityManager 
      */
     private $em;
-    
-    public function __construct($em = NULL, $house_id = NULL )
+    public function __construct($em)
     {
-        $this->house_id = $house_id;
         $this->em = $em;
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-
-        if($this->house_id)
-        $qb = $this->em->getRepository('HOfficeAdminBundle:House\Apartment')
-                                    ->createQueryBuilder('a')
-                                    ->where("a.house = :house_id")
-                                    ->setParameter("house_id", $this->house_id)
-                                    ->orderBy('a.name', 'ASC');      
-        
         $builder
-//            ->add('parent_id')
             ->add('kod')
             ->add('serial_number')
             ->add('registered')
-//            ->add('user', null, array('required'=>NULL))
-//            ->add('apartment', null, array('required'=>NULL))
-            ->add('sale')
-            ->add('user_id', 'hidden',
-                    array('required'=>NULL, 
-                        'attr' => array(
-                            'class' => 'search_user_id' )))
-            ->add('user', 'text',
-                    array('required'=>NULL, 
-                        'attr' => array(
-                            'class' => 'entity_search w250px',
-                            'data-link' => ".search_user_id",
-                            'data-type-link' => "input",
-                            'data-route' => "ajax_search_user"
-                            )));
-/*            ->add('house_id', 'hidden',
-                    array('required'=>NULL, 
-                        'attr' => array(
-                            'class' => 'search_house_id' )))                    
-            ->add('house', 'text',
-                    array('required'=>NULL, 
-                        'attr' => array(
-                            'class' => 'entity_search w250px',
-                            'data-link' => ".search_house_id",
-                            'data-type-link' => "input",
-                            'data-route' => "ajax_search_house",
-                            'data-after-search' => '.loadApartametData'
-                            )))       
-            ->add('load_apartament_data', 'hidden', 
-                    array('required'=>NULL, 
-                        'attr' => array(
-                            'class' => 'loadApartametData',
-                            'data-link' => ".search_apartment_id",
-                            'data-route' => "ajax_search_apartment"
-                        )))  ;
-*/          if(false){
-               $builder->add('apartment', 'entity', array(
-                                'required' => NULL,
-                                'class' => 'HOfficeAdminBundle:House\Apartment',
-                                'property' => 'name',
-                                'query_builder' => $qb,
-                                'attr' => array(
-                                    'class' => 'search_apartment_id' )
-                                )) ;
-            }  
-            else 
-            {
-                $builder->add('apartment_id', 'choice', array(
-                                'required' => NULL,
-                                'attr' => array(
-                                    'class' => 'search_apartment_id' )
-                                )) ;
-            }
-                
-        ;
+            ->add('sale', 'text', array('required'=>NULL));
+                        
+
         $languages = $this->getLanguages();     
          
         foreach($languages as $k => $lang ){
@@ -104,7 +40,7 @@ class ContractType extends AbstractType
         }
         
     }
-    private function getLanguages()
+    protected function getLanguages()
     {
         $locale = LanguageHelper::getLocale();
         $lngs = LanguageHelper::getLanguages();
